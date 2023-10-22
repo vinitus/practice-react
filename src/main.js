66288ff3c1deab9b4d46ad61b1ef17bdc161b0ea
vinitus <tlsdnrng@gmail.com>
2023-10-22 16:39:36 +0900

    모듈화하기

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
 
 const rootNode = document.getElementById('root');
 
-ReactDOM.createRoot(rootNode).render(React.createElement('div', null, 'Hi, React!'));
+ReactDOM.createRoot(rootNode).render(App());
