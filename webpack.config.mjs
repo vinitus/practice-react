52e7fce21069bcf5d50a018b58220f0394e41757
vinitus <tlsdnrng@gmail.com>
2023-10-23 19:52:41 +0900

    config는 동적으로 작동할 필요가 없어짐

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -16,49 +16,44 @@ const tsLoader = {
 };
 
 // 웹팩 설정 function
-const config = (_, args) => {
-  const { mode } = args;
-  const isDevelopment = mode === 'development';
-
-  return {
-    entry: './src/main.tsx', // entry는 진입점이다.
-    // 현재 디렉토리가 __dirname에 저장되니까, dist 디렉토리에 bundle.js로 저장
-    output: {
-      filename: 'bundle.js',
-      path: __dirname + '/dist',
-    },
-    // css 파일에 대한 loader 설정
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: [MiniCssExtractPlugin.loader, 'css-loader'],
-        },
-        {
-          test: /\.[jt]sx?$/,
-          loader: 'esbuild-loader',
-          options: {
-            target: 'es2015',
-          },
-        },
-      ],
-    },
-    // 플러그인 설정
-    plugins: [
-      new HtmlWebpackPlugin({ template: './src/index.html', filename: 'index.html' }),
-      new MiniCssExtractPlugin({ filename: 'style.css', chunkFilename: '[id].css' }),
-    ],
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.jsx'], // extension는 import를 할 때 확장자를 생략할 수 있도록하는 옵션
-    },
-    devServer: {
-      static: {
-        directory: __dirname + '/public',
+const config = {
+  entry: './src/main.tsx', // entry는 진입점이다.
+  // 현재 디렉토리가 __dirname에 저장되니까, dist 디렉토리에 bundle.js로 저장
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/dist',
+  },
+  // css 파일에 대한 loader 설정
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
-      compress: true,
-      port: 3001,
+      {
+        test: /\.[jt]sx?$/,
+        loader: 'esbuild-loader',
+        options: {
+          target: 'es2015',
+        },
+      },
+    ],
+  },
+  // 플러그인 설정
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html', filename: 'index.html' }),
+    new MiniCssExtractPlugin({ filename: 'style.css', chunkFilename: '[id].css' }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'], // extension는 import를 할 때 확장자를 생략할 수 있도록하는 옵션
+  },
+  devServer: {
+    static: {
+      directory: __dirname + '/public',
     },
-  };
+    compress: true,
+    port: 3001,
+  },
 };
 
 export default config;
