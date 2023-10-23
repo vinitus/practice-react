6c32025152c032d8cc658360c90e2ddd149142f4
vinitus <tlsdnrng@gmail.com>
2023-10-23 18:59:35 +0900

    웹팩 실행시 모드를 통해 typescript parser를 동적 할당

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -9,57 +9,63 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// 웹팩 설정 Object
-const config = {
-  entry: './src/main.tsx', // entry는 진입점이다.
-  // 현재 디렉토리가 __dirname에 저장되니까, dist 디렉토리에 bundle.js로 저장
-  output: {
-    filename: 'bundle.js',
-    path: __dirname + '/dist',
-  },
-  // css 파일에 대한 loader 설정
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        loader: 'babel-loader',
-        exclude: '/node_modules',
-        test: /\.(js|jsx|ts|tsx)$/,
-        options: {
-          rootMode: 'upward',
-        },
-      },
-      {
-        rules: [
-          {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-          },
-        ],
-      },
-    ],
-  },
-  // 플러그인 설정
-  plugins: [
-    new HtmlWebpackPlugin({ template: './src/index.html', filename: 'index.html' }),
-    new MiniCssExtractPlugin({ filename: 'style.css', chunkFilename: '[id].css' }),
-  ],
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'], // extension는 import를 할 때 확장자를 생략할 수 있도록하는 옵션
-  },
-  devServer: {
-    static: {
-      directory: __dirname + '/public',
-    },
-    compress: true,
-    port: 3000,
-  },
-  // 이 번들링 파일이 개발모드인지, 프로덕션인지를 알려주기 위한 mode key
-  // mode를 CLI 인수로 전달하기 위한 주석처리
-  // mode: 'production',
+const tsLoader = {
+  test: /\.tsx?$/,
+  use: 'ts-loader',
+  exclude: /node_modules/,
 };
+
+// 웹팩 설정 function
+const config = (_, args) => {
+  const { mode } = args;
+  const isDevelopment = mode === 'development';
+
+  return {
+    entry: './src/main.tsx', // entry는 진입점이다.
+    // 현재 디렉토리가 __dirname에 저장되니까, dist 디렉토리에 bundle.js로 저장
+    output: {
+      filename: 'bundle.js',
+      path: __dirname + '/dist',
+    },
+    // css 파일에 대한 loader 설정
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          loader: 'babel-loader',
+          exclude: '/node_modules',
+          test: /\.(js|jsx|ts|tsx)$/,
+          options: {
+            rootMode: 'upward',
+            presets: isDevelopment && ['@babel/preset-typescript'],
+          },
+        },
+        !isDevelopment && {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    // 플러그인 설정
+    plugins: [
+      new HtmlWebpackPlugin({ template: './src/index.html', filename: 'index.html' }),
+      new MiniCssExtractPlugin({ filename: 'style.css', chunkFilename: '[id].css' }),
+    ],
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'], // extension는 import를 할 때 확장자를 생략할 수 있도록하는 옵션
+    },
+    devServer: {
+      static: {
+        directory: __dirname + '/public',
+      },
+      compress: true,
+      port: 3001,
+    },
+  };
+};
+
 export default config;
