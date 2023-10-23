700a90bb4b8218e8070a7bc4710c89bb6776241c
vinitus <tlsdnrng@gmail.com>
2023-10-23 15:35:29 +0900

    typescript 에러 해결

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-const rootNode = document.getElementById('root');
+const rootNode = document.getElementById('root') as HTMLElement;
 
 ReactDOM.createRoot(rootNode).render(<App />);
