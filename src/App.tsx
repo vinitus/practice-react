2cf12c3aafac39d9dced4a10eb92dc7e19252a63
vinitus <tlsdnrng@gmail.com>
2023-10-24 16:13:13 +0900

    css: Header와 Main 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import HeaderComponent from './components/HeaderComponent';
+import styled from 'styled-components';
+
+const Main = styled.main`
+  height: calc(100% - 60px);
+`;
 
 export default function App() {
   return (
     <>
       <HeaderComponent />
-      <div>Hi, React with JSX!</div>
+      <Main>
+        <div>Hi, React with JSX!</div>
+      </Main>
     </>
   );
 }
